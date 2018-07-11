2136056bf3c09e97cf265fdccedbcb10c2b536f3
IsamBenS <isam.ben-soltane@inserm.fr>
2018-07-11 16:35:25 +0200

    Revert "Revert "Source File""
    This reverts commit 2e10a211b27a83cc03d8f6cf37c0f19604940cb8.

---
+++ ModifyFCS.R
@@ -0,0 +1,58 @@
+library(flowCore)
+library(Biobase)
+
+add.keyword.to.fcs <- function(fcs, added.keyword, added.keyword.name)
+{
+    fcs.out <- fcs
+    fcs.out@description <- c(fcs.out@description,added.keyword)
+    names(fcs.out@description) <- c(names(fcs@description),added.keyword.name)
+    
+    return(fcs.out)
+}
+
+write.FCS.CIPHE <- function(fcs, fcs.path)
+{
+    descR <- description(fcs)
+    lapply(c(1:ncol(fcs@exprs)),function(x)
+    {
+        descR[[paste0("$P",x,"R")]] <<- 262144
+    })
+    fcs.out <- flowFrame(fcs@exprs, description = descR)
+    fcs.out@description <- descR
+    
+    write.FCS(fcs.out, fcs.path, delimiter = '#')
+}
+
+
+keyword.exists.FCS <- function(fcs, key.part)
+{
+    key.exists <- F
+    i <- 1
+    while(!key.exists && i <= length(fcs@description))
+    {
+        if(grepl(key.part, names(fcs@description)[i], fixed = T))
+        {
+           key.exists <- T
+        }
+        i <- i+1
+    }
+    
+    return(key.exists)
+}
+
+get.keywords.with.keypart.FCS <- function(fcs, key.part)
+{
+    keywords.list <- c()
+    i <- 1
+    while(i <= length(fcs@description))
+    {
+        if(grepl(key.part, names(fcs@description)[i], fixed = T))
+        {
+            keywords.list <- c(keywords.list,fcs@description[[i]])
+            names(keywords.list)[length(keywords.list)] <- names(fcs@description)[i]
+        }
+        i <- i+1
+    }
+    
+    return(keywords.list)
+}
